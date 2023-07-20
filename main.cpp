3420374536ffcdd3aabaf2f07ac2d754cb486dbf
amankbps <amankbps@gmail.com>
2023-07-20 14:29:08 +0530

    git add .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
 
+
+#include <bits/stdc++.h>
+#include "Model/RubiksCube3dArray.cpp"
+
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    RubiksCube3dArray rubicks3d;
+    rubicks3d.print();
+    rubicks3d.l();
+    rubicks3d.print();
+    if(rubicks3d.isSolved())
+        cout<<"SOLVED\n";
+    else
+        cout<<"NOT SOLVED\n";
+    rubicks3d.lPrime();
+    rubicks3d.print();
+    if(rubicks3d.isSolved())
+        cout<<"SOLVED\n";
+    else
+        cout<<"NOT SOLVED\n";
+
     return 0;
 }
